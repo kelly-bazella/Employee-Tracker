9fce6a352ffdbecd9164b68b5ee08a1a47ea72eb
Kelly Bazella <kelly.bazella@gmail.com>
2020-03-13 12:22:28 -0500

    all options functional

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Employee-Tracker
+
+## How to use
+
+## User Story
+
+`As a business owner
+I want to be able to view and manage the departments, roles, and employees in my company
+So that I can organize and plan my business
+`
+
+## Purpose
+
+## GIF
--- index.js
+++ index.js
@@ -21,15 +21,76 @@ connection.connect(function(err){
 
 
 function addDepartment(){
-    var sql = "INSERT into department (name) VALUES (?)"
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the name of the department you would like to add?",
+        name: "dept"
+    }]).then(({dept}) => {
+        var sql = "INSERT into department (name) VALUES (?)"
+        connection.query(sql, dept, function (err, res){
+            if(err) throw err;
+            viewDepartments();
+        })
+    })
 }
 
 function addEmployee(){
+    inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your employee's first name?",
+        name: "firstname"
+    },
+    {
+        type: "input",
+        message: "What is your employee's last name?",
+        name: "lastname"
+    },
+    {
+        type: "input",
+        message: "What is your employee's role id?",
+        name: "roleid"
+    },
+    {
+        type: "input",
+        message: "What is your employee's manager id?",
+        name: "managerid"
+    }
+]).then(data =>{
     var sql = "INSERT into employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)"
+    // same as dept but dept in connection query [x,y,z,f]
+    connection.query(sql, [data.firstname, data.lastname, data.roleid, data.managerid], function (err, res){
+        if(err) throw err;
+    viewEmployees();
+})
+})
 }
 
+
 function addRole(){
-    var sql = "INSERT into role (title, salary, department_id) VALUES (?.?,?)"
+    inquirer.prompt([
+        {
+            type:"input",
+            message: "What is the title of the job?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the salary for this position?",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "What is the department id number?",
+            name: "deptid"
+        }
+    ]).then(data => {
+        var sql = "INSERT into role (title, salary, department_id) VALUES (?,?,?)"
+        connection.query(sql, [data.title, data.salary, data.deptid], function (err, res){
+            if(err) throw err;
+        viewRoles();
+    })
+    })
 }
 
 function viewDepartments(){
@@ -57,6 +118,28 @@ function viewRoles(){
         console.table(res);
         start();
     })
+}
+
+function updateEmployeeRole(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the role ID number?",
+            name: "rolenumber"
+        },
+        {
+            type: "input",
+            message: "What is the ID number where you want to make this change?",
+            name: "idnumber"
+        }
+    ]).then(data => {
+        var sql = "UPDATE employee SET role_id = ? WHERE id = ?;"
+        connection.query(sql, [data.rolenumber, data.idnumber], function (err, res){
+            if(err) throw err;
+        viewEmployees();
+    })
+    })
+    
 }
 
 var starterQuestion = 
@@ -102,8 +185,12 @@ function start () {inquirer.prompt(starterQuestion).then(data => {
     else if(data.initialQuestions == "Add role"){
         addRole();
     }
+    else if(data.initialQuestions == "Update employee role"){
+        updateEmployeeRole();
+    }
 });
 }
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
